35a96bf02418858a2b24f86b9a3f8b4cf5d6c426
kieselll <popovicikirill95@gmail.com>
2025-09-24 20:13:27 +0300

    REIMPORT
    Self explanatory ._.

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -7,28 +7,28 @@ var _target_zoom
 var _zoom_step: = 1.8
 
 func _process(delta):
-  if Input.is_action_pressed("up"):
-    move_local_y(delta * (-1 * speed) / get_zoom().x)
-  if Input.is_action_pressed("left"):
-    move_local_x(delta * (-1 * speed) / get_zoom().x)
-  if Input.is_action_pressed("right"):
-    move_local_x(delta * (speed) / get_zoom().x)
-  if Input.is_action_pressed("down"):
-    move_local_y(delta * (speed) / get_zoom().x)
-  if Input.is_action_pressed("zoom_in"):
-    set_zoom((get_zoom() + (Vector2(zoom_speed, zoom_speed) / 1000)).clamp(Vector2(0.5, 0.5), Vector2(5, 5)))
-    reset_smoothing()
-  if Input.is_action_pressed("zoom_out"):
-    set_zoom((get_zoom() - (Vector2(zoom_speed, zoom_speed) / 1000)).clamp(Vector2(0.5, 0.5), Vector2(5, 5)))
-    reset_smoothing()
+	if Input.is_action_pressed("up"):
+		move_local_y(delta * (-1 * speed) / get_zoom().x)
+	if Input.is_action_pressed("left"):
+		move_local_x(delta * (-1 * speed) / get_zoom().x)
+	if Input.is_action_pressed("right"):
+		move_local_x(delta * (speed) / get_zoom().x)
+	if Input.is_action_pressed("down"):
+		move_local_y(delta * (speed) / get_zoom().x)
+	if Input.is_action_pressed("zoom_in"):
+		set_zoom((get_zoom() + (Vector2(zoom_speed, zoom_speed) / 1000)).clamp(Vector2(0.5, 0.5), Vector2(5, 5)))
+		reset_smoothing()
+	if Input.is_action_pressed("zoom_out"):
+		set_zoom((get_zoom() - (Vector2(zoom_speed, zoom_speed) / 1000)).clamp(Vector2(0.5, 0.5), Vector2(5, 5)))
+		reset_smoothing()
 
-  if _target_zoom and _target_zoom != zoom:
-    zoom = zoom.lerp(_target_zoom, delta * 6)
-    if zoom.x < _target_zoom.x + 0.01 and zoom.x > _target_zoom.x - 0.01:
-      zoom = _target_zoom
+	if _target_zoom and _target_zoom != zoom:
+		zoom = zoom.lerp(_target_zoom, delta * 6)
+		if zoom.x < _target_zoom.x + 0.01 and zoom.x > _target_zoom.x - 0.01:
+			zoom = _target_zoom
 
 func _unhandled_input(event):
-  if event.is_action_pressed("zoom_in_mouse"):
-    _target_zoom = (zoom * _zoom_step).clamp(min_zoom, max_zoom)
-  elif event.is_action_pressed("zoom_out_mouse"):
-    _target_zoom = (zoom / _zoom_step).clamp(min_zoom, max_zoom)
+	if event.is_action_pressed("zoom_in_mouse"):
+		_target_zoom = (zoom * _zoom_step).clamp(min_zoom, max_zoom)
+	elif event.is_action_pressed("zoom_out_mouse"):
+		_target_zoom = (zoom / _zoom_step).clamp(min_zoom, max_zoom)
--- scripts/control.gd
+++ scripts/control.gd
@@ -5,12 +5,12 @@ var minutes
 var hours
 
 func _ready() -> void :
-  pass
+	pass
 
 func _process(delta: float) -> void :
-  time = $"../handlers/process_handler".time
-  $CanvasLayer / clock / Sprite2D / ColorRect3.rotation = $CanvasLayer / clock / Sprite2D / ColorRect3.rotation + (0.00523598775 * delta)
-  $CanvasLayer / clock / Sprite2D / ColorRect2.rotation = $CanvasLayer / clock / Sprite2D / ColorRect2.rotation + (0.06283185299999999 * delta)
-  minutes = roundi((time - floori(time) + (floori(time) %100)) / 1.666666666)
-  hours = int(time / 100)
-  $CanvasLayer / clock / Label.text = "%02d:%02d" % [hours, minutes]
+	time = $"../handlers/process_handler".time
+	$CanvasLayer / clock / Sprite2D / ColorRect3.rotation = $CanvasLayer / clock / Sprite2D / ColorRect3.rotation + (0.00523598775 * delta)
+	$CanvasLayer / clock / Sprite2D / ColorRect2.rotation = $CanvasLayer / clock / Sprite2D / ColorRect2.rotation + (0.06283185299999999 * delta)
+	minutes = roundi((time - floori(time) + (floori(time) %100)) / 1.666666666)
+	hours = int(time / 100)
+	$CanvasLayer / clock / Label.text = "%02d:%02d" % [hours, minutes]
