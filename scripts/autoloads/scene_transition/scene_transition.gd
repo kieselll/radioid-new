c870bdfdb69a4024a642b91ce94487e760c96b3d
kieselll <popovicikirill95@gmail.com>
2026-02-27 20:34:47 +0200

    style/wip: added new startup animations
    Added a duration multiplier
    Added new show dev icon function
    Added fading to start_animation()

--- scripts/autoloads/scene_transition/scene_transition.gd
+++ scripts/autoloads/scene_transition/scene_transition.gd
@@ -6,10 +6,12 @@ extends CanvasLayer
 signal done
 
 
-func start_trans(animated: bool = false):
+func start_trans(animated: bool = false, duration_multiplier : float = 1):
+	anim_player.speed_scale = duration_multiplier
 	anim_player.play("fade_in")
 	if animated:
 		await anim_player.animation_finished
+		anim_player.speed_scale = 1
 		anim_player.play("blow_up_planet")
 		GlobalLogger.write_to_logs(self, "Started animated transition")
 	else:
@@ -17,7 +19,25 @@ func start_trans(animated: bool = false):
 	await anim_player.animation_finished
 	done.emit()
 
+func start_animation(duration_multiplier : float = 1):
+	anim_player.speed_scale = duration_multiplier
+	anim_player.play("fade_in_planet")
+	await  anim_player.animation_finished
+	anim_player.play("blow_up_planet")
+	GlobalLogger.write_to_logs(self, "Started animation")
+	await anim_player.animation_finished
+	done.emit()
 
-func finish_trans():
+func finish_trans(duration_multiplier : float = 1):
+	anim_player.speed_scale = duration_multiplier
 	anim_player.play("fade_out")
 	GlobalLogger.write_to_logs(self, "Stopped transition")
+	await anim_player.animation_finished
+	done.emit()
+
+func show_dev_icon(duration_multiplier : float = 1):
+	anim_player.speed_scale = duration_multiplier
+	anim_player.play("dev_name")
+	GlobalLogger.write_to_logs(self, "Showed dev name and icon")
+	await anim_player.animation_finished
+	done.emit()
