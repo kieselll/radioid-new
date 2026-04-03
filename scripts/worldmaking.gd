a77b4e3b6cc9b9c6ed96ad966ae94714193fee78
kieselll <popovicikirill95@gmail.com>
2026-04-03 18:40:28 +0300

    style: cleaned up worldmaking.gd

--- scripts/worldmaking.gd
+++ scripts/worldmaking.gd
@@ -3,6 +3,9 @@ extends Control
 var fade_in_tween
 var fade_out_tween
 
+@export var create_world_button : Button
+@export var back_button : Button
+
 
 func _ready() -> void:
 	SceneTransition.finish_trans()
@@ -20,34 +23,31 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
-func animate_button(button: Button, scale: Vector2, position: Vector2, color: Color) -> void:
+func animate_button(button : Button, self_scale: Vector2, color: Color) -> void:
 	var tween = create_tween().set_parallel(true).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(button, "scale", scale, 0.3).set_trans(Tween.TRANS_CIRC)
-	tween.parallel().tween_property(button, "position", position, 0.3).set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(button, "scale", self_scale, 0.3).set_trans(Tween.TRANS_CIRC)
 	tween.parallel().tween_property(button.get_theme_stylebox("normal"), "bg_color", color, 0.3)
 
 
 func _on_back_mouse_entered() -> void:
 	animate_button(
-		$Container/Back,
+		back_button,
 		Vector2(1.2, 1.2),
-		$Container/Back.position - Vector2(4.9, 4.1),
 		Color("c5b405ff")
 	)
 
 
 func _on_back_mouse_exited() -> void:
-	animate_button($Container/Back, Vector2(1, 1), Vector2(0, 0), Color("1a1a1a"))
+	animate_button(back_button, Vector2(1, 1), Color("1a1a1a"))
 
 
 func _on_create_world_mouse_entered() -> void:
 	animate_button(
-		$"Container2/Create World",
+		create_world_button,
 		Vector2(1.2, 1.2),
-		$"Container2/Create World".position - Vector2(15, 4.1),
 		Color("86c900")
 	)
 
 
 func _on_create_world_mouse_exited() -> void:
-	animate_button($"Container2/Create World", Vector2(1, 1), Vector2(0, 0), Color("1a1a1a"))
+	animate_button(create_world_button, Vector2(1, 1), Color("1a1a1a"))
