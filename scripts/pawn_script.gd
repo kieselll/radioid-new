343420892365ddfe3b0052952567b732eac2ae03
kieselll <popovicikirill95@gmail.com>
2026-05-07 21:41:01 +0300

    style/fix: renamed pawn script + fixed debug input problem

--- scripts/pawn_script.gd
+++ scripts/pawn_script.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
+# CRITICAL PLEASE CLEAN THIS SHIT UP
+
 @export var characteristics: PawnStats
 @onready var state_machine = $StateMachine
 @onready var action_machine = $ActionMachine
-@onready var movement_component = $MovementComponent
-@onready var building_component = $BuildingComponent
 @onready var ability_manager = $AbilityManager
 @onready var decision_maker = $DecisionMaker
 
@@ -17,14 +17,17 @@ var left_front_texture = load("res://man_diag3.png")
 var right_back_texture = load("res://man_diag2.png")
 var left_back_texture = load("res://man_diag.png")
 
-signal full_ready
 signal died
 
+var _movement_component : MovementComponent
+var _building_component : BuildingComponent
 
-func _ready() -> void:
+func initialize(movement_component : MovementComponent, building_component : BuildingComponent) -> void:
+	_movement_component = movement_component
+	_movement_component.setup(self)
+	_building_component = building_component
+	_building_component._parent = self
 	$Label.text = name
-	var children := get_children()
-
 
 func rotate_sprite(direction: Vector2):
 	if direction.angle() > -0.3839724 and direction.angle() < 0.3839724:
@@ -44,23 +47,27 @@ func rotate_sprite(direction: Vector2):
 	elif direction.angle() > -1.16937 and direction.angle() < -0.3839724:
 		$Sprite2D.texture = right_back_texture
 
-
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and Input.is_physical_key_pressed(KEY_Q):
-		print(characteristics.stats["tiredness"])
-		print(characteristics.stats["health"])
-		print($"../TileMap".local_to_map(position))
-	elif Input.is_physical_key_pressed(KEY_U):
-		characteristics.stats["health"] = 5
-	elif Input.is_physical_key_pressed(KEY_K):
-		characteristics.stats["health"] = 0
-	elif Input.is_physical_key_pressed(KEY_KP_ADD):
-		characteristics.abilities["building"] = clampi(
-			characteristics.abilities["building"] + 1, 0, 9
-		)
-		print(characteristics.abilities["building"])
-	elif Input.is_physical_key_pressed(KEY_KP_SUBTRACT):
-		characteristics.abilities["building"] = clampi(
-			characteristics.abilities["building"] - 1, 0, 9
-		)
-		print(characteristics.abilities["building"])
+	if not OS.is_debug_build():
+		return
+
+	if event is InputEventKey and event.pressed and not event.echo:
+		match event.physical_keycode:
+			KEY_Q:
+				print(characteristics.stats["tiredness"])
+				print(characteristics.stats["health"])
+				print($"../TileMap".local_to_map(position))
+			KEY_U:
+				characteristics.stats["health"] = 5
+			KEY_K:
+				characteristics.stats["health"] = 0
+			KEY_KP_ADD:
+				characteristics.abilities["building"] = clampi(
+					characteristics.abilities["building"] + 1, 0, 9
+				)
+				print(characteristics.abilities["building"])
+			KEY_KP_SUBTRACT:
+				characteristics.abilities["building"] = clampi(
+					characteristics.abilities["building"] - 1, 0, 9
+				)
+				print(characteristics.abilities["building"])
