697f453e3e326ddd92e8183eb650c6bc5f09122e
kieselll <popovicikirill95@gmail.com>
2026-05-07 22:41:54 +0300

    feat: added tick types for BaseComponent
    Now the pawn script knows whether the component should tick normally, physically or shouldn't

--- scripts/components/base_component.gd
+++ scripts/components/base_component.gd
@@ -2,6 +2,14 @@
 extends RefCounted
 class_name BaseComponent
 
+enum {
+	none,
+	regular,
+	physics,
+}
+
+var tick_type = none
+
 @abstract func tick(delta : float) -> void
 
 # Add more functions here later
