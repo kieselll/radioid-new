c4065d0082819f96a678bfb18610b38dc5667c01
kieselll <popovicikirill95@gmail.com>
2026-05-16 21:08:04 +0300

    feat: created entity_manager.gd

---
+++ scripts/handlers/entity_manager.gd
@@ -0,0 +1,31 @@
+extends Node
+
+enum types
+{
+	pawn,
+}
+
+class entity:
+	var type : types
+	var node : Node
+
+	@warning_ignore("shadowed_variable")
+	func _init(type : types, node : Node) -> void:
+		self.type = type
+		self.node = node
+
+var entities : Dictionary
+
+var entity_pool
+
+func summon_pawn(characteristics : PawnStats) -> String:
+	var pawn_scene : PackedScene = GlobalRef.get_scene(GlobalRef.scenes_enum.pawn)
+	var pawn_node = pawn_scene.instantiate()
+	pawn_node.initialize(MovementComponent.new(), BuildingComponent.new())
+	pawn_node.characteristics = PawnStats.new(PawnStats.personalities.AMBIVERT, {}, [])
+	add_child(pawn_node)
+
+	return ""
+
+func delete_entity(entity_id : int):
+	pass
