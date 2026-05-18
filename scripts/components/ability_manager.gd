bd8efc1946f78c92429699fa2207578fc39e3093
kieselll <popovicikirill95@gmail.com>
2026-05-18 22:03:15 +0300

    feat: migrated AbilityManager to BaseComponent

--- scripts/components/ability_manager.gd
+++ scripts/components/ability_manager.gd
@@ -1,14 +1,20 @@
 @icon("res://textures/editor_icons/skills.svg")
-extends Node
+extends BaseComponent
 class_name AbilityManager
 ## A pawn component that manages a pawn's abilities in building, medical, etc...
 
 enum ability_names { BUILD, COOK, SHOOT, FIGHT }
 
-var ability_name_map = {&"build_action": ability_names.BUILD}
+var ability_name_map = {ActionMachine.action_types.build: ability_names.BUILD}
+var _parent : CharacterBody2D
 
 # CRITICAL POPULATE THIS ^^^
 
+func tick(delta : float) -> void:
+	pass
+
+func setup(parent : CharacterBody2D) -> void:
+	_parent = parent
 
 func xp_to_level(xp: int):
 	return floor(log((xp + 10) / 1000.) / log(1.4))
@@ -31,5 +37,5 @@ func get_ability_level(ability_name: ability_names) -> int:
 
 
 func add_xp(ability: ability_names, amount: int):
-	GlobalLogger.write_to_logs(self, "Added xp: %d to %s" % [amount, ability])
+	GlobalLogger.write_to_logs(_parent, "Added xp: %d to %s" % [amount, ability])
 	_ability_xp[ability] += amount
