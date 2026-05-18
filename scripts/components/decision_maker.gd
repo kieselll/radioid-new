739379cbb2816bf464418feaa0a176fff2ec22ff
kieselll <popovicikirill95@gmail.com>
2026-05-18 21:31:50 +0300

    fix: migrated DecisionMaker to use action types instead of names

--- scripts/components/decision_maker.gd
+++ scripts/components/decision_maker.gd
@@ -15,17 +15,17 @@ const TEMP_BASE_PRIORITY = 5  #CRITICAL
 
 
 class QueuedAction:
-	var action_name: StringName
+	var action_type: ActionMachine.action_types
 	var priority: float
 	var args: Dictionary
 
-	func _init(_action_name: StringName, _priority: float, _args: Dictionary = {}) -> void:
-		action_name = _action_name
+	func _init(_action_type: ActionMachine.action_types, _priority: float, _args: Dictionary = {}) -> void:
+		action_type = _action_type
 		priority = _priority
 		args = _args
 
 
-var _action_queue: Array[QueuedAction] = [QueuedAction.new(&"wander_action", 0)]
+var _action_queue: Array[QueuedAction] = [QueuedAction.new(ActionMachine.action_types.wander, 0)]
 # ^^^ Should always stay here as priority 0, is default value
 var _current_action: QueuedAction
 var _action_machine: ActionMachine
@@ -44,18 +44,19 @@ func setup(parent : CharacterBody2D) -> void:
 	_movement_component = _parent.movement_component
 	_current_action = _action_queue[0]
 	_astar = _parent.get_node(GlobalRef.get_handler(GlobalRef.handlers_enum.pathfinder))
-	_action_machine.start_action(_current_action.action_name, _current_action.args)
+	_action_machine.start_action(_current_action.action_type, _current_action.args)
 
 
-## Adds a new QueuedAction to the [member _action_queue], in the format of a class. Takes in an action_name (must be a valid action_name, like &"wander", a wanted priority for the action,
+
+## Adds a new QueuedAction to the [member _action_queue], in the format of a class. Takes in an action_type (must be a valid action_type, a wanted priority for the action,
 ## and, optionally, arguments to the action, like the target for [MoveAction]. Doesn't have a [StateMachine] counterpart, because actions ([BaseAction]) already fulfill this need.
-func add_action_to_queue(action_name: StringName, priority: int, action_args: Dictionary = {}):
+func add_action_to_queue(action_type: ActionMachine.action_types, priority: int, action_args: Dictionary = {}):
 	GlobalLogger.write_to_logs(
-		self, "Added %s to queue with base priority: %f" % [action_name, priority]
+		_parent, "Added %s to queue with base priority: %f" % [action_type, priority]
 	)
 	for action_index in range(_action_queue.size() - 1, -1, -1):
 		if _action_queue[action_index].priority < priority:
-			_action_queue.insert(action_index, QueuedAction.new(action_name, priority, action_args))
+			_action_queue.insert(action_index, QueuedAction.new(action_type, priority, action_args))
 			return
 
 
@@ -68,7 +69,7 @@ func add_action_to_queue(action_name: StringName, priority: int, action_args: Di
 ## [color=red][b] NOT DONE, PLEASE MAKE IT HAPPEN[/b][/color][br][br]
 ## The function takes in the base priority, location and the emotion modifier [color=red][b] LAST ONE NOT DONE, PLEASE MAKE IT HAPPEN[/b][/color]
 func calculate_action_priority_modifier(
-	action_name: StringName,
+	action_type: StringName,
 	base_priority: int,
 	location: Vector4i = Vector4i.MAX,
 	emotion_modifier: float = 1
@@ -80,10 +81,10 @@ func calculate_action_priority_modifier(
 		else 0
 	)
 	var skill_level: int
-	var priority = 0 if action_name == &"wander_action" else base_priority
-	if _ability_manager.action_name_to_ability_name(action_name):
+	var priority = 0 if action_type == &"wander_action" else base_priority
+	if _ability_manager.action_type_to_ability_name(action_type):
 		skill_level = _ability_manager.get_ability_level(
-			_ability_manager.action_name_to_ability_name(action_name)
+			_ability_manager.action_type_to_ability_name(action_type)
 		)
 		if skill_level < 0 and base_priority < 5:
 			skill_modified = skill_level * _negative_skill_weight
@@ -96,24 +97,24 @@ func calculate_action_priority_modifier(
 
 
 func _on_action_machine_action_done() -> void:
-	if _current_action.action_name != &"wander_action":
+	if _current_action.action_type != ActionMachine.action_types.wander:
 		_action_queue.erase(_current_action)
 	_action_queue.sort_custom(_queue_sort)
 	_current_action = _action_queue[0]
-	_action_machine.start_action(_current_action.action_name, _current_action.args)
+	_action_machine.start_action(_current_action.action_type, _current_action.args)
 
 
 func _queue_sort(a, b):
 	if a.args.keys().has(&"target"):
 		a.priority = calculate_action_priority_modifier(
-			a.action_name, TEMP_BASE_PRIORITY, a.args[&"target"]
+			a.action_type, TEMP_BASE_PRIORITY, a.args[&"target"]
 		)
 	else:
-		a.priority = calculate_action_priority_modifier(a.action_name, TEMP_BASE_PRIORITY)
+		a.priority = calculate_action_priority_modifier(a.action_type, TEMP_BASE_PRIORITY)
 	if b.args.keys().has(&"target"):
 		b.priority = calculate_action_priority_modifier(
-			b.action_name, TEMP_BASE_PRIORITY, b.args[&"target"]
+			b.action_type, TEMP_BASE_PRIORITY, b.args[&"target"]
 		)
 	else:
-		b.priority = calculate_action_priority_modifier(b.action_name, TEMP_BASE_PRIORITY)
+		b.priority = calculate_action_priority_modifier(b.action_type, TEMP_BASE_PRIORITY)
 	return a.priority > b.priority
