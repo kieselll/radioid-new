459ece3cb0d757d2ad34ad388ee833b02a40dac7
kieselll <popovicikirill95@gmail.com>
2026-06-21 11:49:58 +0300

    fix: started deleting redundant entity files

--- scripts/autoloads/Saver.gd
+++ scripts/autoloads/Saver.gd
@@ -153,6 +153,8 @@ func save_chunk(coords : Vector2i):
 	var entity_manager : EntityManager = get_node(GlobalRef.get_handler(GlobalRef.handlers_enum.entity_manager))
 	if entity_manager.serialize_chunk(coords).is_empty()\
 	and not FileAccess.file_exists(_current_save_path + "/entities/" + str(coords) + ".json"): return
+	elif FileAccess.file_exists(_current_save_path + "/entities/" + str(coords) + ".json"):
+		DirAccess.remove_absolute(_current_save_path + "/entities/" + str(coords) + ".json")
 	var _entity_file_access = FileAccess.open(_current_save_path + "/entities/" + str(coords) + ".json", FileAccess.WRITE)
 	_entity_file_access.store_string(JSON.stringify(entity_manager.serialize_chunk(coords)))
 
