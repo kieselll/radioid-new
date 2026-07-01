5851134c2501ec0cd8595a2e953bd4328485d02c
kieselll <popovicikirill95@gmail.com>
2026-07-01 11:34:26 +0300

    fix/wip: fixed building component

--- scripts/components/building_component.gd
+++ scripts/components/building_component.gd
@@ -14,7 +14,7 @@ func setup(parent : CharacterBody2D) -> void:
 	_parent = parent
 
 func build(coords: Vector4i, id: int, time: float = 5):
-	var _data = BuildableDB.get_tile(id)
+	var _data := BuildableDB.get_tile(id)
 	if not _data:
 		GlobalLogger.write_to_logs(_parent, "[ERROR]: Could not build tile with id: %d!" % id)
 		GlobalLogger.open_log_file()
@@ -24,14 +24,14 @@ func build(coords: Vector4i, id: int, time: float = 5):
 	)
 	progressbar.z_index = 20
 	progressbar.size = Vector2(100, 10)
-	progressbar.position = (GridUtils.chunk_coord_to_tile_coord(coords) - Vector2i(12, 26))
-	_parent.get_node(GlobalRef.get_tilemap_layer_path(_data.layer)).add_child(progressbar)
+	progressbar.position = (GridUtils.chunk_coord_to_world_coord(coords))
+	_parent.get_node(GlobalRef.get_handler(GlobalRef.handlers_enum.building_agent)).add_child(progressbar)
 	var progressbar_tween = _parent.create_tween()
 	progressbar_tween.tween_property(progressbar, "value", 100, time)
 	await progressbar_tween.finished
 	_parent.get_node(GlobalRef.get_handler(GlobalRef.handlers_enum.building_agent)).fill_array(
 		[coords], _data, false
 	)
-	_parent.get_node(GlobalRef.get_tilemap_layer_path(_data.queued_layer)).erase_cell(coords)
+	_parent.get_node(GlobalRef.get_handler(GlobalRef.handlers_enum.building_agent)).erase_tiles([coords], _data.queued_layer)
 	progressbar.queue_free()
 	finished_building.emit()
