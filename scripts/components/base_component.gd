df3aa07c94ef59f8f04e688050a1b3e49c589adf
kieselll <popovicikirill95@gmail.com>
2026-07-21 12:51:12 +0300

    style: refactored BaseComponent

--- scripts/components/base_component.gd
+++ scripts/components/base_component.gd
@@ -2,14 +2,14 @@
 extends RefCounted
 class_name BaseComponent
 
-enum {
+enum tick_types{
 	none,
 	regular,
 	physics,
 }
 
-var tick_type = none
-var ticking = false
+var tick_type: tick_types = tick_types.none
+var ticking: bool = false
 
 @abstract func tick(delta : float) -> void;
 
