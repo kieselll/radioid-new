c95fa5c0a36dcbe23b061c898f58905fa71d49cf
kieselll <popovicikirill95@gmail.com>
2026-07-30 11:41:55 +0300

    style: refactored build state

--- scripts/components/states/build_state.gd
+++ scripts/components/states/build_state.gd
@@ -4,42 +4,42 @@ extends BaseState
 
 var _movement_component: MovementComponent
 var _building_component: BuildingComponent
-var _parent : StateMachine
-var owner : CharacterBody2D
+var _parent: StateMachine
+var owner: BaseEntity
 
-const state_name = &"build_state"
+const state_name: StringName = &"build_state"
 
-func setup(state_machine : StateMachine):
+func setup(state_machine: StateMachine) -> void:
 	_parent = state_machine
 	owner = _parent.owner
 	_movement_component = owner.movement_component
 	_building_component = owner.building_component
 
 func start(args: Dictionary = {}) -> void:
+	print(typeof(args["target"]))
+	var target: Variant = args.get("target")
 	assert(
-		args["target"] is Vector4i,
+		target is Vector4i,
 		(
 			'BuildState of %s recieved start(), but has no argument "target" of type Vector4i.'
 			% owner.name
 		)
 	)
+	var build_target: Vector4i = target
 
-	assert(
-		args["id"] is int or args["id"] is float,
-		'BuildState of %s recieved start(), but has no argument "id" of type int.' % owner.name
-	)
+	var id_value: int = args.get("id")
 	GlobalLogger.write_to_logs(owner, "Started building")
-	var _neighbor_tiles := GridUtils.get_neighbor_tiles(args["target"], true)
+	var _neighbor_tiles: Array[Vector4i] = GridUtils.get_neighbor_tiles(build_target, true)
 	assert(
 		_neighbor_tiles.has(_movement_component.get_local_position()),
 		"%s tried to build a non-adjacent tile" % owner.name
 	)
-	_building_component.build(args["target"], int(args["id"]))
+	_building_component.build(build_target, id_value)
 	await _building_component.finished_building
 	stop()
 	done.emit()
 
 
-func stop():
+func stop() -> void:
 	GlobalLogger.write_to_logs(owner, "Stopped building")
 	_active = false
