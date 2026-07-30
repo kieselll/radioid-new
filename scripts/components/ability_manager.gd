e1d4bf3b559ab70aec92e70f1e343d695de2aed1
kieselll <popovicikirill95@gmail.com>
2026-07-30 11:43:26 +0300

    style: refactored ability manager

--- scripts/components/ability_manager.gd
+++ scripts/components/ability_manager.gd
@@ -3,21 +3,23 @@ extends BaseComponent
 class_name AbilityManager
 ## A pawn component that manages a pawn's abilities in building, medical, etc...
 
-enum ability_names { BUILD, COOK, SHOOT, FIGHT }
+enum ability_names { BUILD, COOK, SHOOT, FIGHT}
 
-var ability_name_map = {ActionMachine.action_types.build: ability_names.BUILD}
-var _parent : CharacterBody2D
+var ability_name_map: Dictionary[ActionMachine.action_types, ability_names] = {
+	ActionMachine.action_types.build: ability_names.BUILD
+}
+var _parent: BaseEntity
 
 # CRITICAL POPULATE THIS ^^^
 
-func tick(delta : float) -> void:
+func tick(_delta: float) -> void:
 	pass
 
-func setup(parent : CharacterBody2D) -> void:
+func setup(parent: BaseEntity) -> void:
 	_parent = parent
 
-func xp_to_level(xp: int):
-	return floor(log((xp + 10) / 1000.) / log(1.4))
+func xp_to_level(xp: int) -> int:
+	return floori(log((xp + 10) / 1000.) / log(1.4))
 
 
 @export var _ability_xp: Dictionary[ability_names, int] = {
@@ -28,13 +30,17 @@ func xp_to_level(xp: int):
 }
 
 
-func action_type_to_ability_name(action_type: ActionMachine.action_types):
-	return ability_name_map[action_type] if ability_name_map.keys().has(action_type) else null
+func action_type_to_ability_name(action_type: ActionMachine.action_types) -> ability_names:
+	assert(ability_name_map.keys().has(action_type))
+	return ability_name_map[action_type]
 
 
-func action_name_to_ability_name(action_name: StringName):
+func action_name_to_ability_name(action_name: StringName) -> Variant:
 	if ActionMachine.action_types.has(action_name):
-		return action_type_to_ability_name(ActionMachine.action_types[action_name])
+		var action_type := (
+			ActionMachine.action_types[action_name] as ActionMachine.action_types
+		)
+		return action_type_to_ability_name(action_type)
 	return null
 
 
@@ -42,6 +48,6 @@ func get_ability_level(ability_name: ability_names) -> int:
 	return xp_to_level(_ability_xp[ability_name])
 
 
-func add_xp(ability: ability_names, amount: int):
+func add_xp(ability: ability_names, amount: int) -> void:
 	GlobalLogger.write_to_logs(_parent, "Added xp: %d to %s" % [amount, ability])
 	_ability_xp[ability] += amount
