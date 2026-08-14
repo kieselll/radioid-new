dec0c43f351884b6be1511cc8185de382ab8d44e
kieselll <popovicikirill95@gmail.com>
2026-08-14 23:47:56 +0300

    chore: refactored ConfigDB

--- scripts/autoloads/ConfigDB.gd
+++ scripts/autoloads/ConfigDB.gd
@@ -54,9 +54,10 @@ func write_dict_to_settings() -> void:
 	if FileAccess.file_exists(_settings_file_path):
 		_settings_file.save(_settings_old_file_path)
 		GlobalLogger.write_to_logs(self, "Saved previous version as backup")
-	for section in _cached_settings.keys():
-		for key in _cached_settings[section].keys():
-			_settings_file.set_value(section, key, _cached_settings[section][key])
+	for section: String in _cached_settings.keys():
+		var section_dict: Dictionary[String, Variant] = _cached_settings[section]
+		for key: String in section_dict.keys():
+			_settings_file.set_value(section, key, section_dict[key])
 	var err := _settings_file.save(_settings_file_path)
 	if err != OK:
 		GlobalLogger.write_to_logs(self, "Couldn't save settings, error: %s" % str(err))
@@ -67,7 +68,7 @@ func load_settings() -> Dictionary:
 	GlobalLogger.write_to_logs(self, "Loading settings...")
 	if not _cached_settings:
 		_cached_settings = _default_settings.duplicate(true)
-		var err = _settings_file.load(_settings_file_path)
+		var err: Error = _settings_file.load(_settings_file_path)
 		if err != OK:
 			GlobalLogger.write_to_logs(self, "Couldn't load settings file, error: %s, trying backup..." %str(err))
 			err = _settings_file.load(_settings_old_file_path)
@@ -83,35 +84,44 @@ func load_settings() -> Dictionary:
 	GlobalLogger.write_to_logs(self, "Settings parsed and stored in cache")
 	return _cached_settings
 
-func get_setting(section: String, key: String, default = null):
-	if _cached_settings.has(section) and _cached_settings[section].has(key):
-		return _cached_settings[section][key]
+func get_setting(section: String, key: String, default: Variant = null) -> Variant:
+	if _cached_settings.has(section):
+		var section_dict: Dictionary[String, Variant] = _cached_settings[section]
+		if section_dict.has(key):
+			return section_dict[key]
 	return default
 
-func alter_setting(section : String, key : String, value: Variant):
-	if _cached_settings.has(section) and _cached_settings[section].has(key):
-		_cached_settings[section][key] = value
-		apply_setting(section, key)
+func alter_setting(section : String, key : String, value: Variant) -> void:
+	if _cached_settings.has(section):
+		var section_dict: Dictionary[String, Variant] = _cached_settings[section]
+		if section_dict.has(key):
+			_cached_settings[section][key] = value
+			apply_setting(section, key)
 
 func apply_settings() -> void:
-	for section in _cached_settings.keys():
-		for key in _cached_settings[section].keys():
+	for section: String in _cached_settings.keys():
+		var section_dict: Dictionary[String, Variant] = _cached_settings[section]
+		for key: String in section_dict.keys():
 			apply_setting(section, key)
 
 func apply_setting(section: String, key: String) -> void:
-	var value = get_setting(section, key)
+	var value: Variant = get_setting(section, key)
 	if section == "graphics":
 		match key:
 			"frame_rate_limit":
+				@warning_ignore("unsafe_call_argument")
 				Engine.max_fps = int(value)
 			"window_type":
-				var window_mode := int(value)
+				@warning_ignore("unsafe_cast")
+				var window_mode := value as Window.Mode
 				if window_mode in [Window.MODE_WINDOWED, Window.MODE_FULLSCREEN, Window.MODE_EXCLUSIVE_FULLSCREEN]:
 					get_window().mode = window_mode
 	elif section == "audio":
+		@warning_ignore("unsafe_cast")
 		var bus_name := {"main_volume": "Master", "sfx_volume": "SFX", "music_volume": "Music"}.get(key, "") as String
 		var bus_index := AudioServer.get_bus_index(bus_name)
 		if bus_index >= 0:
+			@warning_ignore("unsafe_call_argument")
 			var normalized_volume := clampf(float(value) / 100.0, 0.0, 1.0)
 			AudioServer.set_bus_volume_db(bus_index, linear_to_db(normalized_volume))
 			AudioServer.set_bus_mute(bus_index, is_zero_approx(normalized_volume))
