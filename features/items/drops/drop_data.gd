b626a7be6c1aaae0ce3419ba357a8dd76432c3e0
kieselll <popovicikirill95@gmail.com>
2026-09-13 21:02:16 +0300

    feat: added drop functionality to drop_data
    should technically work now

--- features/items/drops/drop_data.gd
+++ features/items/drops/drop_data.gd
@@ -4,3 +4,10 @@ class_name DropData
 ## or when an entity is killed or interacted with
 
 @export var pools: Array[ItemPool] = []
+
+func drop(position: Vector4i) -> void:
+	var chunk: Chunk = GlobalRef.get_chunk(Vector2i(position.x, position.y))
+	var item_manager: ItemManager = chunk.item_manager
+	for pool: ItemPool in pools:
+		for i: ItemManager.BaseItem in pool.get_items():
+			item_manager.add_item(i.id, Vector2i(position.z, position.w), 1, i.data, true)
